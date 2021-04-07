97c9836bf9421d83a946a25e0180dbabb5680734
Ranfis Francisco <ranfis.francisco@gmail.com>
2021-04-07 19:08:27 +0000

    got server to run and start db

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 import os
-from flask import Flask, send_from_directory
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, send_from_directory, json
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import models
 
-app = Flask(__name__, static_folder='./build/static')
+APP = Flask(__name__, static_folder='./build/static')
+
+load_dotenv(find_dotenv())
+
+# Point SQLAlchemy to your Heroku database
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DB = SQLAlchemy(APP)
+
+Person = models.define_person_class(DB)
+Favorite = models.define_favorite_class(DB)
+Chat = models.define_chat_class(DB)
+DB.create_all()
 
 
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
 
-app.run(
+APP.run(
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 '''Defines classes used in database.'''
 
-def define_user_class(db):
+def define_person_class(db):
     '''Returns class definition using database instance.'''
-    class User(db.Model):
+    class Person(db.Model):
         '''Class for User data model'''
         email = db.Column(db.String(80), primary_key=True)
         username = db.Column(db.String(80))
@@ -11,13 +11,29 @@ def define_user_class(db):
         def __repr__(self):
             return '<User %r Score {}>'.format(self.score) % self.username
 
-    return User
+    return Person
     
 def define_favorite_class(db):
     '''Returns class definition of Favorite model using database instance.'''
     class Favorite(db.Model):
-        email = db.Column(db.String, db.ForeignKey('User.email'), primary_key=True)
+        email = db.Column(db.String, db.ForeignKey('person.email'), primary_key=True)
         media = db.Column(db.String(80), primary_key=True)
         
         def __repr__(self):
             return '<Favorite Instance: User %r Media {}>'.format(self.email) % self.media
+            
+    return Favorite
+            
+def define_chat_class(db):
+    
+    class Chat(db.Model):
+        
+        email = db.Column(db.String, db.ForeignKey('person.email'), primary_key=True)
+        message = db.Column(db.String)
+        media = db.Column(db.String(10000))
+        timestamp = db.Column(db.DateTime(), primary_key=True)
+        
+        def __repr__(self):
+            return '<Chat Instance: email {} media {} timestamp {}>'.format(self.email, self.media, self.timestamp)
+        
+    return Chat
