c0195896df7bb55b27c06dfbe16d2030f3047bd3
Ranfis Francisco <ranfis.francisco@gmail.com>
2021-04-07 19:19:02 +0000

    made datetime use current time as default (untested)

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql import func
+
 '''Defines classes used in database.'''
 
 def define_person_class(db):
@@ -6,7 +8,7 @@ def define_person_class(db):
         '''Class for User data model'''
         email = db.Column(db.String(80), primary_key=True)
         username = db.Column(db.String(80))
-        joinDate = db.Column(db.String(30))
+        joinDate = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
         def __repr__(self):
             return '<User %r Score {}>'.format(self.score) % self.username
@@ -31,7 +33,7 @@ def define_chat_class(db):
         email = db.Column(db.String, db.ForeignKey('person.email'), primary_key=True)
         message = db.Column(db.String)
         media = db.Column(db.String(10000))
-        timestamp = db.Column(db.DateTime(), primary_key=True)
+        timestamp = db.Column(db.DateTime(timezone=True), server_default=func.now())
         
         def __repr__(self):
             return '<Chat Instance: email {} media {} timestamp {}>'.format(self.email, self.media, self.timestamp)
