b3aa80a87c500927652c6a96a4d2f86a9ffc5403
Akulkumar Patel <avp76@njit.edu>
2021-04-08 19:50:42 +0000

    Initial commit; Creating the branch of of main for search feature

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,6 @@
-const API_KEY = " ";
+// const API_KEY = " ";
+const API_KEY =`${process.env.REACT_APP_API_KEY}`;
+console.log(API_KEY);
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -9,6 +11,6 @@ const requests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-}
+};
 
 export default requests;
