1ae57e9572210ef0ea67beaf84aa24eacc5e2b95
Ibraam Melika <ibraam2009@gmail.com>
2021-04-16 20:57:11 -0400

    wrote code to handle movie search

--- src/Page.js
+++ src/Page.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from "react";
 
 import YouTube from "react-youtube"
 import movieTrailer from "movie-trailer"
 
 function Page({name}) {
+
+    const [trailerURL, setTrailerURL] = useState("");
+
+    //const theName = {name}
+
+    const handleClick = (movie) => {
+        movieTrailer("avengers" || "")
+        .then((url) => {
+            const urlParams = new URLSearchParams(new URL(url).search)
+            setTrailerURL(urlParams.get("v"))
+        })
+        .catch((error) => console.log(error))
+    }
+
+    handleClick({name})
+    console.log(trailerURL)
+
+    //console.log(trailer)
 
     //you need this for the youtube video size
     const opts = {
