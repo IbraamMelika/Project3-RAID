f0b2013af5188a3b6a4bafb2ff366146c9a9713a
Ranfis Francisco <ranfis.francisco@gmail.com>
2021-04-17 21:34:20 +0000

    added fav button compnent file

---
+++ src/FavoriteButton.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+import YouTube from "react-youtube"
+import movieTrailer from "movie-trailer"
+
+function FavoriteButton({email, media}) {
+    const handleClick = (movie) => {
+       
+    }
+
+    console.log("Email " + email + " Media " + media);
+    
+
+
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default FavoriteButton
