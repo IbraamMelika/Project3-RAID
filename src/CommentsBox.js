acedfcfa497059d7fcad742327f2b0ceebc037fb
IbraamMelika <ec2-user@ip-172-31-91-214.ec2.internal>
2021-04-29 18:06:28 +0000

    debugging useState

--- src/CommentsBox.js
+++ src/CommentsBox.js
@@ -1,10 +1,10 @@
-import React, { useState }from "react";
+import React, { useState, useEffect }from "react";
 //import { addComment, getAllComments } from './App.js';
 
 
 function CommentsBox({name, userEmail}) {
   
-  const [commentState, setCommentState]=useState([]);
+  const [commentState, setCommentState]=useState([{ email: "", message: "", timestamp: ""}]);
   
   function getAllComments(media){
     media = encodeURIComponent(media);
@@ -23,48 +23,65 @@ function CommentsBox({name, userEmail}) {
         const comments = responseData.comments
         setCommentState(comments)
         console.log(comments)
+        console.log(typeof comments)
+      });
+      
+  }
+  
+  function addComment(email, media, message){
+  email = encodeURIComponent(email);
+  media = encodeURIComponent(media);
+  message = encodeURIComponent(message);
+  
+  const url = "/api/v1/comment";
+  const data = JSON.stringify({'email': email, 'media': media, 'message': message});
+  
+  fetch(url, {
+      method: 'POST',
+       headers: {
+          'Content-Type': 'application/json'
+         },
+         body: data
+       })
+     .then(response => {
+        return response.json();
+      }).then(responseData => {
+        console.log(responseData);
       });
   }
   
   //this works
   //"Stowaway"
-  //getAllComments("Stowaway");
-  //console.log(typeof commentState);
+  
+  useEffect(() => {
+    getAllComments("Stowaway");
+    console.log(commentState);
+    }, []);
   
 
-    /*
-    name = encodeURIComponent(name);
-    userEmail = encodeURIComponent(userEmail);
-    
-    //const url = "/api/v1/comment?media=" + media;
-    
-    
-    addComment(userEmail,name,"test message")
-    getAllComments(name)
-    //console.log(typeof getAllComments(name))
-    //console.log(getAllComments(name))
-    */
+  
     
     return(
         <div>
-        <h1 className="title">Comments Section</h1>
-        
-        <form onSubmit="function-goes-here-to-submit">
-          
+        <h1 className="title">Kindly leave your thoughts below</h1>
+        <form onSubmit={"add the addComment function here"}>
+          <div className="field">
+            <div className="control">
+              <input type="text" className="input" name="name" placeholder="Your name"/>
+            </div>
+          </div>
           <div className="field">
             <div className="control">
               <textarea className="textarea" name="comment" placeholder="Add a comment"></textarea>
             </div>
           </div>
-          
           <div className="field">
             <div className="control">
               <button className="button is-primary">Submit</button>
             </div>
           </div>
-          
         </form>
-        </div>
+      </div>
     )
 }
 
