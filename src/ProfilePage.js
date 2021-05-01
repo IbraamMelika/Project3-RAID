5f26c34043591aea2e715ecc8a64c223de96a9eb
Andre Alvarados <ec2-user@ip-172-31-54-247.ec2.internal>
2021-05-01 20:01:29 +0000

    Eslint react hook

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -62,6 +62,7 @@ function ProfilePage(prop) {
   
   useEffect(() => {
     getUserInfoByEmail(prop.userEmail);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
   function clearDesc() {
